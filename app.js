9ad881d030415ca93b778090b4ac51e823e1b07c
TheBusinessPanda <mastermrinank@gmail.com>
2025-05-09 19:57:52 +0530

    Added basic routing

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+let app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('index.html');
+});
+
+let PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on PORT ${PORT}`);
+});
